f4beea2df5f30ca28c7bb70956e9aafa106b5609
EJacquet42 <edouardjacquet83@gmail.com>
2024-12-24 11:44:15 +0000

    IPv4&6

--- src/verif_ip.php
+++ src/verif_ip.php
@@ -1,17 +1,27 @@
 <?php
 
-require_once 'functions.php';
 require_once 'get_ip.php';
+require_once 'ip_functions.php';
 
 $ip = getIpAddress();
+
+if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+    $ipType = "IPv4";
+} elseif (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+    $ipType = "IPv6";
+} else {
+    $ipType = "Inconnue";
+}
+
+echo "L'adresse IP détectée est de type $ipType.<br>";
 
 if (ipFrance($ip)) {
     echo "L'adresse IP $ip est en France.";
     include './home.php';
 } else {
     echo "L'adresse IP $ip n'est pas en France.";
-    include './oups.php';
+    include './ip_invalide.php';
 }
-
 ?>
+
 
